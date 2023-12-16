7be4f85c99cbf2b1d8f25e792eae4eef2082cc57
Marcos Vinicius <vi.niso65@gmail.com>
2023-12-15 21:58:27 -0300

    feat: finish question 1

---
+++ app/Http/Controllers/location.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use function Laravel\Prompts\error;
+
+class location extends Controller
+{
+   
+    public function location()
+    {
+
+        $locations = array
+        ([
+            'Brasil'    => 'Brasilia',
+            'USA'       => 'Washington',
+            'Chile'     => 'Santiago',
+            'Estonia'   => 'Tallin',
+            'Irlanda'   => 'Dublin'
+        ]);
+
+        arsort($locations[0]);
+        $locations = array_reverse($locations[0], true);
+
+        foreach($locations as $key => $val) {
+            dump($key );
+        }
+        
+        return view('questao1', ['data' => $locations]);
+    }
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\location;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/questao1', [location::class, 'location'])->name('location');
+Route::get('/questao2', [location::class, 'location'])->name('location');
+Route::get('/questao3', [location::class, 'location'])->name('location');
+Route::get('/questao4', [location::class, 'location'])->name('location');
+Route::get('/questao5', [location::class, 'location'])->name('location');
+Route::get('/questao6', [location::class, 'location'])->name('location');
+Route::get('/questao7', [location::class, 'location'])->name('location');
